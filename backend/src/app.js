0ffaa274cd5ab885e9a6018c3e5341b0d84cad98
bssathwik <balabhadrasaisathwik@gmail.com>
2024-08-11 18:00:29 +0530

    all set for project

---
+++ backend/src/app.js
@@ -0,0 +1,45 @@
+import cors from 'cors';
+import express from 'express';
+import { connectToDB,db } from "./db.js";
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+
+app.post('/signin', async(req, res) => {
+   const userdata= await db.collection("ast").findOne({gmail:req.body.gmail})
+   if(!userdata){
+    return res.json({
+        status:"fail",
+        message:"user unsucessfully sign"
+    })}
+   if(req.body.password!==userdata.password){
+    return res.json({
+        status:"fail",
+        message:"password missmatch"
+        })
+   }
+   else(
+   res.json({
+    status:"success",
+    message:"successfully login"
+   }))
+
+   
+   
+})
+app.post('/signup',async(req,res)=>{
+    await db.collection("ast").insertOne({gmail:req.body.gmail,phone:req.body.phone,password:req.body.password,register:req.body.register})
+    .then((result)=>{
+        res.json(result)
+    })
+    .catch((e)=>console.log(e))
+})
+
+connectToDB(() => {
+    app.listen(9000, () => {
+        console.log("server running at 9000");
+    })
+})
+
